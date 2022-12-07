86b713743f7abc50bb09ea0e3b9523970c9009b3
karimbedu <kpurushotham.777@gmail.com>
2022-12-07 23:18:41 +0530

    Bhanu Changed

--- src/javademo/FirstProgramInJava.java
+++ src/javademo/FirstProgramInJava.java
@@ -4,7 +4,7 @@ public class FirstProgramInJava {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello GitHub");
+		System.out.println("Hello Bhanu");
 		System.out.println("Hello Selenium");
 
 	}
